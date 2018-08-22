ad200e5a7ff72e9b6e323a5b683223aa22e9f434
alexandra.oliveira@gmal.com <alexandra.oliveira@cern.ch>
2018-08-22 06:27:09 +0200

    some polishing

--- submit_HHsignal.sh
+++ submit_HHsignal.sh
@@ -2,14 +2,15 @@
 
 
 finalState="4V"
+#finalState="4T"
+#finalState="2T2V"
 cmsbase="/afs/cern.ch/work/a/acarvalh/delphes_main/Delphes_CMS/CMSSW_8_1_0/src/"
 localFolder="/afs/cern.ch/work/a/acarvalh/delphes_main/HH_4taus_delphes/"
 channelFoldersubmit="Folder_HHTo"$finalState"_submit"
 channelFolderResult="Folder_HHTo"$finalState"/"
 
-# Out_part_9.root
-
 mkdir $channelFoldersubmit
+mkdir $channelFolderResult
 ### here you have the total count of events -- as double check
 exec 3<> $channelFoldersubmit/submit_HH_signal_totalCount.txt
     echo "#!/bin/bash" >&3
@@ -17,14 +18,14 @@ exec 3<> $channelFoldersubmit/submit_HH_signal_totalCount.txt
     echo "eval \`scram runtime -sh\`" >&3
     echo "cd "$localFolder >&3
     echo "python With_Electrons.py --input \"/eos/cms/store/user/acarvalh/delphes_HH_4tau/HHTo"$finalState"/*\" --onlyCount " >&3
-# Close file
 exec 3>&-
-#cd $channelFoldersubmit
-#bsub  -q 8nm -J jobCount < submit_HH_signal_totalCount.txt
-#cd $localFolder
+cd $channelFoldersubmit
+echo "Submit count total events"
+bsub  -q 8nm -J jobCount < submit_HH_signal_totalCount.txt
+cd $localFolder
 
 ## here you process ~10 files by time on batch
-for ii in {10..40}
+for ii in {10..49}
 do
   fileOut=$channelFolderResult"Out_part_"$ii".root"
   if [[ -f $fileOut && -s $fileOut ]]; then
@@ -39,7 +40,7 @@ do
         echo "python With_Electrons.py --input \"/eos/cms/store/user/acarvalh/delphes_HH_4tau/HHTo"$finalState"/*_"$ii"*.root\"  --nameOut "$ii >&3
     exec 3>&-
     cd $channelFoldersubmit
-    bsub  -q 8nm -J job$ii < submit_HH_signal_$ii.txt
+    bsub  -q 1nh -J job$ii < submit_HH_signal_$ii.txt
     cd $localFolder
   fi
 done
@@ -60,7 +61,7 @@ do
         echo "python With_Electrons.py --input \"/eos/cms/store/user/acarvalh/delphes_HH_4tau/HHTo"$finalState"/*_"$ii"*.root\"  --nameOut "$ii >&3
     exec 3>&-
     cd $channelFoldersubmit
-    bsub  -q 8nm -J job$ii < submit_HH_signal_$ii.txt
+    bsub  -q 1nh -J job$ii < submit_HH_signal_$ii.txt
     cd $localFolder
   fi
 done
@@ -81,7 +82,7 @@ do
         echo "python With_Electrons.py --input \"/eos/cms/store/user/acarvalh/delphes_HH_4tau/HHTo"$finalState"/*_"$ii".root\"  --nameOut "$ii >&3
     exec 3>&-
     cd $channelFoldersubmit
-    bsub  -q 8nm -J job$ii < submit_HH_signal_$ii.txt
+    bsub  -q 1nh -J job$ii < submit_HH_signal_$ii.txt
     cd $localFolder
   fi
 done
