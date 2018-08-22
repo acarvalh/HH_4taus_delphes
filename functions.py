ff39fe1e2d5d55f71b841f1f72afdbf5a1d0a684
alexandra.oliveira@gmal.com <alexandra.oliveira@cern.ch>
2018-08-22 10:09:46 +0200

    read function

--- functions.py
+++ functions.py
@@ -10,4 +10,33 @@ def isbtagged(jets, GenB) :
 
 sign = lambda a: 1 if a>0 else -1 if a<0 else 0
 
-
+def read_channel(Categories, channelLocal, totalEvtLocal) :
+    sumAllTest = 0
+    toRead = glob.glob(str(options.input)+"/Folder_"+channelLocal+"/*.root")
+    dict_data = OrderedDict()
+    for category in Categories :
+        dict_data[category] = [pandas.DataFrame(columns=["evtcounter"])]
+        for file in toRead :
+            try: tfile = ROOT.TFile(file)
+            except :
+            	print ('file ', file,' is corrupt')
+            	continue
+            try: tree = tfile.Get(category)
+            except :
+            	print ('Tree for category ', category,' is corrupt')
+            	continue
+            if tree is not None :
+                try : chunk_arr = tree2array(tree, branches=featuresToPlot)
+                except : continue # tree was empty
+                else :
+                    chunk_df = pandas.DataFrame(chunk_arr) #
+                    ### we can manipulate some branches here
+                    #chunk_df["MissingET_eta"]=abs(chunk_df["MissingET_eta"])
+                    #chunk_df["MissingET_phi"]=abs(chunk_df["MissingET_phi"])
+                    dict_data[category][0]=dict_data[category][0].append(chunk_df, ignore_index=True)
+            else : print ("file "+file+"was empty")
+            tfile.Close()
+        print ("Category ",category, "of channel", channelLocal,  "had ", len(dict_data[category][0]["evtcounter"]),"events")
+        sumAllTest+=len(dict_data[category][0]["evtcounter"])
+    print ("read",sumAllTest, "events, and we should have ",totalEvtLocal," events ",100*(float(sumAllTest)/float(totalEvtLocal)),"% were processed")
+    return dict_data
